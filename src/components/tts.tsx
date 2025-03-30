ab480befe2f0fb09109b4992dc455e67ce328591
pauloreyes <aijin02@gmail.com>
2025-03-30 15:48:17 +0800

    Added conditional rendering in the history component

--- src/components/tts.tsx
+++ src/components/tts.tsx
@@ -116,8 +116,12 @@ const TTS = () => {
 	return (
 		<div className='bg-white p-6 rounded-lg shadow-md w-[80%] mx-auto'>
 			<div className='flex gap-6'>
-				<div className='w-1/2'>
-					<div className='space-y-4'>
+				<div
+					className={
+						history.length > 0 ? "w-1/2" : "w-full flex justify-center items-center"
+					}
+				>
+					<div className='space-y-4 w-full'>
 						<div>
 							<label className='block text-sm font-medium text-gray-700 mb-2 text-left'>
 								Select VO Actor
@@ -191,8 +195,8 @@ const TTS = () => {
 					</div>
 				</div>
 
-				<div className='w-1/2 overflow-y-auto max-h-[500px]'>
-					{history.length > 0 && (
+				{history.length > 0 && (
+					<div className='w-1/2 overflow-y-auto max-h-[500px]'>
 						<div className='mt-4'>
 							<h3 className='text-sm font-medium text-gray-700 mb-2'>History</h3>
 							<ul className='space-y-2'>
@@ -256,7 +260,7 @@ const TTS = () => {
 														fill='currentColor'
 														className='size-6'
 													>
-														<path d='M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375Z' />
+														<path d='M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375Z' />
 														<path
 															fill-rule='evenodd'
 															d='m3.087 9 .54 9.176A3 3 0 0 0 6.62 21h10.757a3 3 0 0 0 2.995-2.824L20.913 9H3.087ZM12 10.5a.75.75 0 0 1 .75.75v4.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 1 1 1.06-1.06l1.72 1.72v-4.94a.75.75 0 0 1 .75-.75Z'
@@ -270,8 +274,8 @@ const TTS = () => {
 								})}
 							</ul>
 						</div>
-					)}
-				</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
