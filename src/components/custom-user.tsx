0ca9d6fc405f69cc89948515d989f0d33534284f
pauloreyes <aijin02@gmail.com>
2025-03-30 15:08:08 +0800

    Centered the user button and salutation

--- src/components/custom-user.tsx
+++ src/components/custom-user.tsx
@@ -1,31 +1,32 @@
-'use client';
+"use client";
 
-import { SignedIn, UserButton, useUser } from '@clerk/nextjs';
-import React from 'react';
-import { useSidebar } from '@/components/ui/sidebar';
+import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import React from "react";
+import { useSidebar } from "@/components/ui/sidebar";
 
 const CustomUserButton = () => {
-  const { user } = useUser();
-  const { state } = useSidebar(); // Get the sidebar state
+	const { user } = useUser();
+	const { state } = useSidebar(); // Get the sidebar state
 
-  return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <UserButton />
-      {user && state === 'expanded' && ( // Conditionally render the name
-        <span style={{ marginLeft: '8px' }}>
-          Hi, {user.firstName}!
-        </span>
-      )}
-    </div>
-  );
+	return (
+		<div style={{ display: "flex", alignItems: "center" }}>
+			<UserButton />
+			{user &&
+				state === "expanded" && ( // Conditionally render the name
+					<span style={{ marginLeft: "8px" }}>Hi, {user.firstName}!</span>
+				)}
+		</div>
+	);
 };
 
 const CustomUser = () => {
-  return (
-    <SignedIn>
-      <CustomUserButton />
-    </SignedIn>
-  );
+	return (
+		<SignedIn>
+			<div className='flex items-center justify-center'>
+				<CustomUserButton />
+			</div>
+		</SignedIn>
+	);
 };
 
 export default CustomUser;
